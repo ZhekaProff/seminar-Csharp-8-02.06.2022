da79738ae7dced60e3dfcfb8288b4f306cc4236b
ZhekaProff <Zhekakad@ya.ru>
2022-06-02 18:26:40 +0900

    решение задич с текстом и кол букв

---
+++ Lesson/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.Write("Введите текст: ");
+string text = Console.ReadLine() ?? "0";
+text = text.ToLower(); 
+string collection = "";
+for (int i = 0; i < text.Length; i++)
+{
+    if( !collection.Contains(text[i]))
+        collection += text[i];
+}
+
+string [,] matrix = new string [collection.Length,2];
+
+for ( int i = 0; i < collection.Length; i++)
+{
+    matrix[i, 0] = Convert.ToString(collection[i]);
+    matrix[i, 1] = "0";
+}
+
+
+for ( int i = 0; i < text.Length; i++)
+{
+    for (int j = 0; j < collection.Length; j++)
+        if(text[i] == collection[j])
+        {
+            matrix[j,1] = Convert.ToString(Convert.ToInt32(matrix[j, 1]) + 1);
+        }
+}
+for(int j = 0; j < collection.Length; j++)
+    Console.WriteLine($"символ '{matrix[j,0]}' в тексте встречается {matrix[j,1]} раз");
+   
+
+
+
+
